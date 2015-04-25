91a186be548a2c48de4829ca3634ccc2aab4cb77
Andrew Fritz <andrew.fritz86@gmail.com>
2015-04-25 14:55:05 -0400

    move static files

--- app.js
+++ app.js
@@ -1,3 +1,28 @@
-var request = require("request");
-var marked = require("marked");
 var express = require("express");
+var fs = require("fs");
+var app = express();
+
+app.use(express.static(__dirname + '/public'));
+
+
+app.get('/', function (req, res) {
+  var page = fs.readFileSync("./index.html", "utf8");
+  res.send(page);
+});
+
+
+
+
+
+
+
+
+
+var server = app.listen(3000, function () {
+
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('Example app listening at http://%s:%s', host, port);
+
+});
