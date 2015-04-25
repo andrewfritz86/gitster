7110b17aeb056e81a240f4b745b8d3c5c2ca2edc
Andrew Fritz <andrew.fritz86@gmail.com>
2015-04-25 14:55:37 -0400

    yoooo

---
+++ client.js
@@ -0,0 +1,24 @@
+console.log("client linked")
+//dom references
+var button = $("#submitbutton")
+var form = $("#mainform")
+var template = $("#template").text()
+
+//listeners
+button.on("click", function(event){
+    $("#container").empty();
+    $.ajax({
+        url: "https://api.github.com/users/"+ form.val() + "/followers",
+        type: "GET"
+    }).done(function(data){
+        console.log(data)
+        data.forEach(function(e){
+            var name = e.login
+            var avatar = e.avatar_url
+            var type = e.type
+            var url = e.html_url
+            var rendered = Mustache.render(template, {name: name, avatar: avatar, type:type, url: url})
+            $("#container").append(rendered);
+        })
+    })
+})
