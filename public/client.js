7c8cf593f09b1f38f2070fd93888cc2979ea7255
Andrew Fritz <andrew.fritz86@gmail.com>
2015-04-28 13:45:42 -0400

    add drag

--- public/client.js
+++ public/client.js
@@ -29,3 +29,19 @@ clearbutton.on("click", function(event){
     console.log("fired clear");
     $("#container").empty();
 })
+
+
+interact('.tap-target')
+  .on('tap', function (event) {
+    event.currentTarget.classList.toggle('switch-bg');
+    event.preventDefault();
+  })
+  .on('doubletap', function (event) {
+    event.currentTarget.classList.toggle('large');
+    event.currentTarget.classList.remove('rotate');
+    event.preventDefault();
+  })
+  .on('hold', function (event) {
+    event.currentTarget.classList.toggle('rotate');
+    event.currentTarget.classList.remove('large');
+  });
